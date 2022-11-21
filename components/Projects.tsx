51d47381485d9c91b7b600e1a7e351dd6afccf48
Kyronne <kyronne.parkes@gmail.com>
2022-11-21 23:56:20 +0000

    Optimising for mobile

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,8 +18,8 @@ function Projects({ projects }: Props) {
             Projects</h3>
          <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
             {projects?.map((project, i) => (
-                <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:-44 h-screen'>
-                    <motion.img className='w-xl xl:max-w-3xl'
+                <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center p-20 md:-44 h-screen'>
+                    <motion.img className='w-xl'
                         initial={{
                             y: -300,
                             opacity: 0,
