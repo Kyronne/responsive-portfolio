90db7a5cf3e98af12c2441739fe0078cf65fa23a
Kyronne <kyronne.parkes@gmail.com>
2022-11-21 20:49:46 +0000

    Optimising for mobile

--- components/About.tsx
+++ components/About.tsx
@@ -14,11 +14,10 @@ function About({ pageInfo }: Props) {
     transition={{duration: 1.5}}
     whileInView={{ opacity: 1 }}
     className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-        <div className="px=0 md:px-10">
         <h3 className="absolute top-24 uppercase tracking-[15px] text-gray-500 text-2xl">
             About
         </h3>
-        </div>
+        
         <motion.img
         initial={{
             x: -200,
