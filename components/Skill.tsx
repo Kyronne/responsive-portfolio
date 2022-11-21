b8dde01c07a214115d8d47535c4e3099c3ac9992
Kyronne <kyronne.parkes@gmail.com>
2022-11-21 21:21:41 +0000

    Optimising for mobile

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -17,7 +17,7 @@ function skill({ skill, directionLeft}: Props) {
       transition={{duration: 1}}
       whileInView={{ opacity: 1, x: 0  }}
       src={urlFor(skill?.image).url()}
-      className="rounded-full border object-contain border-gray-500 w-15 h-15 md:h-28 md:w-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+      className="rounded-full border object-scale-down border-gray-500 w-15 h-15 md:h-28 md:w-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
     />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300
       ease-in-out group-hover:hg-bg-black h-15 w-15 md:h-28 md:w-28 xl:w-32 xl:h-32 rounded-full z-0">
