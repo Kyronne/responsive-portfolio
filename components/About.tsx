0979a6242072616582e3522d7b3626829193dae3
Kyronne <kyronne.parkes@gmail.com>
2022-11-21 20:40:55 +0000

    Optimising for mobile

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ function About({ pageInfo }: Props) {
     transition={{duration: 1.5}}
     whileInView={{ opacity: 1 }}
     className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-        <h3 className="absolute top-24 uppercase tracking-[15px] text-gray-500 text-2xl">
+        <h3 className="absolute md:fixed top-24 uppercase tracking-[15px] text-gray-500 text-2xl">
             About
         </h3>
         
