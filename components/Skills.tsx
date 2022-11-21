77e20b37a7e3a083e5a95561d1ffb8c6f73f5929
Kyronne <kyronne.parkes@gmail.com>
2022-11-21 23:14:25 +0000

    Optimising for mobile

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,7 +14,7 @@ function Skills({ skills }: Props) {
     transition={{duration: 1.5}}
     whileInView={{ opacity: 1 }}
     className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 
-    min-h-screen justify-center xl:space-y-0 items-center">
+    min-h-screen justify-center mx-auto xl:space-y-0 items-center">
         <h3 className="absolute top-0 uppercase tracking-[15px] text-gray-500 text-2xl">
             Skills</h3>
         <h3 className="absolute top-8 uppercase tracking-[3px] text-gray-500 text-sm"> 
