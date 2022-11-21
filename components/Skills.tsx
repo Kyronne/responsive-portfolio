9fe80debc4f7dd88aa78066014c2ab3ad93b1508
Kyronne <kyronne.parkes@gmail.com>
2022-11-21 21:00:44 +0000

    Optimising for mobile

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -17,9 +17,10 @@ function Skills({ skills }: Props) {
     min-h-screen justify-center xl:space-y-0 mx-auto items-center">
         <h3 className="absolute top-24 uppercase tracking-[15px] text-gray-500 text-2xl">
             Skills</h3>
-        <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm"> Hover over a skill for current proficiency.</h3>
+        <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm"> 
+        Hover over a skill for current proficiency.</h3>
     
-        <div className="grid grid-cols-3 gap-5" >
+        <div className="grid grid-cols-3 gap-2" >
             {skills?.slice(0, skills.length / 2).map(skill => (
               
             <Skill key={skill._id} skill={skill} />
