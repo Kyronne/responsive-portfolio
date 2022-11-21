0c9ea54f0ab38b3bda7fdb38ae069d5f6434afc4
Kyronne <kyronne.parkes@gmail.com>
2022-11-21 23:39:06 +0000

    Optimising for mobile

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -47,7 +47,7 @@ function ContactMe({}: Props) {
                     </div>
                 </div>
 
-                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-w-[90%]'>
+                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-4/6'>
                     <div className='flex space-x-2'>
                         <input {...register('name')}placeholder="Name" className='contactInput'  type="text" />
                         <input {...register('email')}placeholder="Name" className='contactInput' type="text" />
