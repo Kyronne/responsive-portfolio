cb5bd8f26ef027c1333c88bbb9881bb92ad1e6e9
Kyronne <kyronne.parkes@gmail.com>
2022-11-23 21:01:26 +0000

    Changed project picture width

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -19,7 +19,7 @@ function Projects({ projects }: Props) {
          <div className='relative w-full flex overflow-x-scroll scrollbar scrollbar-thumb-green-500 snap-x snap-mandatory z-20 top-24'>
             {projects?.map((project, i) => (
                 <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:-44 '>
-                    <motion.img className='max-w-xs'
+                    <motion.img className='max-w-xl'
                         initial={{
                             y: -300,
                             opacity: 0,
