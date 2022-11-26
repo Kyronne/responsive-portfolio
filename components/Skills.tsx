1204e0a141e4556da42214ceb49af62c055871c7
Kyronne <kyronne.parkes@gmail.com>
2022-11-26 01:23:32 +0000

    Fix for skills not working on mobile

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,7 +14,7 @@ function Skills({ skills }: Props) {
     transition={{duration: 1.5}}
     whileInView={{ opacity: 1 }}
     className="flex relative flex-col text-center md:text-left xl:flex-row max-w-full xl:px-10 
-    h-fit min-h-screen overflow-auto justify-center mx-auto xl:space-y-0 items-center">
+    h-fit min-h-screen overflow-y-auto overflow-x-hidden justify-center mx-auto xl:space-y-0 items-center">
         <h3 className="absolute top-0 uppercase tracking-[15px] text-gray-500 text-2xl">
             Skills</h3>
         <h3 className="absolute top-8 uppercase tracking-[3px] text-gray-500 text-sm"> 
