358cdf73fd7dcd0db65618f29ad60cc967345bdd
Kyronne <kyronne.parkes@gmail.com>
2022-11-26 00:28:13 +0000

    Made about and skills more responsive for smaller displays

--- components/About.tsx
+++ components/About.tsx
@@ -13,7 +13,7 @@ function About({ pageInfo }: Props) {
     initial={{ opacity: 0 }}
     transition={{duration: 1.5}}
     whileInView={{ opacity: 1 }}
-    className="flex flex-col relative h-fit overflow-auto text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    className="flex flex-col relative h-fit overflow-auto text-center md:text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
         <h3 className="absolute top-0 uppercase tracking-[15px] text-gray-500 text-2xl">
             About
         </h3>
@@ -29,11 +29,11 @@ function About({ pageInfo }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true}}
         src={urlFor(pageInfo?.profilePic).url()}
-        className="-mb-20 md:nb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-tg md:w-64 md:h-95 xl:w-[256px] xl:h-[256px]"/>
+        className="-mb-20 md:nb-0 flex-shrink-0 w-56 h-56 top-30 rounded-full object-cover md:rounded-tg md:w-64 md:h-95 xl:w-[256px] xl:h-[256px]"/>
         <div className="space-y-10 px-0 md:px-10">
-            <h4 className="text-4xl md:text-2xl font-semibold">Here is a <span className="underline">little</span> background
+            <h4 className="text-4xl md:text-2xl top-80 font-semibold">Here is a <span className="underline">little</span> background
             </h4>
-            <p  className="text-base"> {pageInfo?.backgroundInformation}</p>
+            <p  className="text-base overflow-auto"> {pageInfo?.backgroundInformation}</p>
         </div>
     </motion.div>
   )
