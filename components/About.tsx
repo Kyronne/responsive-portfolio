acb76b7fc19905e037ae5248b933f4d80173336d
Kyronne <kyronne.parkes@gmail.com>
2022-11-27 22:09:19 +0000

    Moved Contact Me text to the left to accomodate incon plaement

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,7 @@ function About({ pageInfo }: Props) {
     transition={{duration: 1.5}}
     whileInView={{ opacity: 1 }}
     className="flex flex-col relative h-fit min-h-screen overflow-auto text-center md:text-left md:flex-row lg:w-3/5 max-w-full px-10 justify-evenly lg:justify-center lg:content-center mx-auto items-center">
-        <h3 className="absolute top-0 uppercase tracking-[15px] text-gray-500 text-2xl">
+        <h3 className="absolute space-y-10 top-0 uppercase tracking-[15px] text-gray-500 text-2xl">
             About
         </h3>
         <motion.img
@@ -29,7 +29,7 @@ function About({ pageInfo }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true}}
         src={urlFor(pageInfo?.profilePic).url()}
-        className="mb-20 md:nb-0 flex-shrink-0 w-56 h-56 space-y-10 rounded-full object-cover md:rounded-tg md:w-64 md:h-95 xl:w-[256px] xl:h-[256px]"/>
+        className="mb-20 md:nb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-tg md:w-64 md:h-95 xl:w-[256px] xl:h-[256px]"/>
         <div className="space-y-10 px-0 md:px-10">
             <h4 className="text-4xl md:text-2xl font-semibold">Here is a <span className="underline">little</span> background
             </h4>
