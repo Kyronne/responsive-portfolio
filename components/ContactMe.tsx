27b0fc181c4a36fd27ce95b84911a5082aef8d8b
Kyronne <kyronne.parkes@gmail.com>
2022-11-27 21:57:00 +0000

    Added links for contact me changed about me to display better on smaller displays

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,26 +1,11 @@
 import React from 'react'
 import { CalendarDaysIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
 import { getPageStaticInfo } from 'next/dist/build/analysis/get-page-static-info'
-import { useForm, SubmitHandler } from "react-hook-form";
-
-type Inputs = {
-    name: string;
-    email: string;
-    subject: string;
-    message: string;
-  };
 
 type Props = {}
 
 function ContactMe({}: Props) {
-    const { 
-        register, 
-        handleSubmit, 
-    } = useForm<Inputs>();
-    
-    const onSubmit: SubmitHandler<Inputs> = formData => 
-    {window.location.href = 'mailto:contact@kyronne.com?subject=${formData.subject}&body=Hi, my name is ${formData.message}. ${formData.message} (${formData.email})';
-}
+
   return (
     <div className='h-fit min-h-screen flex relative flex-col text-center md:flex-row 
     px-10 justify-evenly mx-auto items-center'>
@@ -35,7 +20,7 @@ function ContactMe({}: Props) {
                 <div className="space-y-10">
                     <div className='flex items-center space-x-5 justify-center'>
                         <EnvelopeIcon className="text-[#D7D1D1] h-7 w-7 animate-pulse"/>
-                        <p className="text-1xl">contact@kyronne.com</p>
+                        <p className="text-1xl"><a href="mailto:contact@kyronne.com">contact@kyronne.com</a></p>
                     </div>
                     <div className='flex items-center space-x-5 justify-center'>
                         <MapPinIcon className="text-[#D7D1D1] h-7 w-7 animate-pulse"/>
@@ -43,7 +28,7 @@ function ContactMe({}: Props) {
                     </div>
                         <div className='flex items-center space-x-5 justify-center'>
                         <CalendarDaysIcon className="text-[#D7D1D1] h-7 w-7 animate-pulse"/>
-                        <p className="text-1xl">https://calendly.com/kyronne</p>
+                        <p className="text-1xl"><a href="https://calendly.com/kyronne">Book a meeting</a></p>
                     </div>
                 </div>
             </div>
