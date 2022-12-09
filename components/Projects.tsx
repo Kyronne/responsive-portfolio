71108f58b079a2ae9b656790daae62d7b589e89e
Kyronne <kyronne.parkes@gmail.com>
2022-12-09 22:40:57 +0000

    Made projects section work better on pc

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,13 +10,13 @@ type Props = {
 
 function Projects({ projects }: Props) {
     return (
-     <motion.div className="h-fit min-h-screen relative flex flex-col overflow-auto scrollbar scrollbar-thumb-green-500 text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+     <motion.div className="h-fit min-h-screen relative flex flex-col overflow-auto text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
      initial={{ opacity: 0 }}
      transition={{duration: 1.5}}
      whileInView={{ opacity: 1 }} >
         <h3 className="absolute top-0 uppercase tracking-[15px] text-gray-500 text-2xl">
             Projects</h3>
-         <div className='relative w-full h-5/6 flex overflow-auto scrollbar scrollbar-thumb-green-500 snap-x snap-mandatory z-20 top-24'>
+         <div className='absolute w-full h-5/6 flex overflow-auto scrollbar scrollbar-thumb-green-500 snap-x snap-mandatory z-20 top-24'>
             {projects?.map((project, i) => (
                 <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 overflow-y-hidden  items-center justify-center p-5 md:-44 '>
                     <motion.img className='max-w-md lg:max-w-xl'
