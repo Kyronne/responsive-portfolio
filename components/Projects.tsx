e8103264fd2d3b1e7a6e227cfa0035de9ef51f25
Kyronne <kyronne.parkes@gmail.com>
2023-01-10 12:09:04 +0000

    Changed higeht of montion img.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 function Projects({ projects }: Props) {
     return (
-     <motion.div className="h-fit min-h-screen lg:min-h-99/100 relative flex flex-col overflow-auto text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+     <motion.div className="h-fit min-h-screen lg:mx-h-99/100 relative flex flex-col overflow-auto text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
      initial={{ opacity: 0 }}
      transition={{duration: 1.5}}
      whileInView={{ opacity: 1 }} >
