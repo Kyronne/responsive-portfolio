0db7c6ba658e398a5c5004c39b689efb0667abf8
Kyronne <kyronne.parkes@gmail.com>
2023-01-10 12:47:08 +0000

    Changed height of projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 function Projects({ projects }: Props) {
     return (
-     <motion.div className="h-fit min-h-screen lg:mx-h-screen relative flex flex-col overflow-hidden lg:overflow-auto text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+     <motion.div className="h-fit min-h-screen lg:min-h-max relative flex flex-col overflow-hidden lg:overflow-auto text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
      initial={{ opacity: 0 }}
      transition={{duration: 1.5}}
      whileInView={{ opacity: 1 }} >
